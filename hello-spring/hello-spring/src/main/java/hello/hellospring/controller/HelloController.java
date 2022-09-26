db4481eb8d9dfba82675941143b5acfa7f3526dc
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-09-26 17:38:52 +0900

    9-26

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,10 @@ public class HelloController {
         model.addAttribute("data","hello!!!");
         return "hello";
 
+    }
+    @GetMapping("hello-mvc") // 뷰 생성  http://localhost:8080/hello-mvc?name=spring! 라고 치면됨
+    public String helloMvc(@RequestParam("name") String name ,Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
