206a1aaacf97dafd3be2367bdc62035802bc5111
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-11-19 15:17:09 +0900

    구글 로그인 연동 구현

--- springboot_11-12/untitled/src/main/java/org/example/springboot/web/IndexController.java
+++ springboot_11-12/untitled/src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,14 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { //서버 템플릿엔진에서 사용할 수 있는 객체를 저장
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //로그인 성공시 세션에 저장
+        if (user != null) { // 세션에 저장된 값이 있을때 등록
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
